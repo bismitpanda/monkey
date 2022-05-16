79cae93245bddd55c7ab4dfb1fff28364fd905f7
Blood Rogue <64477767+blood-rogue@users.noreply.github.com>
2022-05-16 20:53:45 +0530

    edit

--- object/environment.go
+++ object/environment.go
@@ -2,32 +2,40 @@ package object
 
 func NewLocalEnvironment(outer *Environment) *Environment {
 	env := NewEnvironment()
-	env.Outer = outer
+	env.outer = outer
 
 	return env
 }
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{Store: s, Outer: nil}
+	return &Environment{store: s, outer: nil}
 }
 
 type Environment struct {
-	Store map[string]Object
-	Outer *Environment
+	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
-	obj, ok := e.Store[name]
+	obj, ok := e.store[name]
 
-	if !ok && e.Outer != nil {
-		obj, ok = e.Outer.Get(name)
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
 	}
 
 	return obj, ok
 }
 
 func (e *Environment) Set(name string, val Object) Object {
-	e.Store[name] = val
+	e.store[name] = val
 	return val
+}
+
+func (e *Environment) Outer() *Environment {
+	return e.outer
+}
+
+func (e *Environment) Store() map[string]Object {
+	return e.store
 }
